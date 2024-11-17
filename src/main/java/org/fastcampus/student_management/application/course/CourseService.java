d89ec9728a5ecb459d7dc574b1e8470273a14399
hbbo0e <44goodluck44@gmail.com>
2024-11-17 15:31:33 +0900

    feat: 요일별 수업 조회 service 로직 구현

--- src/main/java/org/fastcampus/student_management/application/course/CourseService.java
+++ src/main/java/org/fastcampus/student_management/application/course/CourseService.java
@@ -26,10 +26,12 @@ public class CourseService {
 
   public List<CourseInfoDto> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
     // TODO: 과제 구현 부분
-    return new ArrayList<>();
+    List<Course> courseList = courseRepository.getCourseDayOfWeek(dayOfWeek);
+    return courseList.stream().map(CourseInfoDto::new).toList();
   }
 
   public void changeFee(String studentName, int fee) {
     // TODO: 과제 구현 부분
+
   }
 }
